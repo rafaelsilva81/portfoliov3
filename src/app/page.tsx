c97ffaa0b7984b7b15a89638606b50b8b1a666e5
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-09-29 21:03:27 -0300

    feat: scroller working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 "use client";
 
 import PageSlider from "@/components/organisms/PageSlider";
+import { useEffect } from "react";
+import { Element, scroller } from "react-scroll";
 
 export default function Home() {
+  const pageOrder = ["hero", "about", "technologies", "projects"];
+  const defaultScrollProps = {
+    duration: 1000,
+    smooth: true,
+  };
+
+  useEffect(() => {
+    // on scroll, let the plugin know so it can adjust the active class
+    window.addEventListener(
+      "wheel",
+      (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const currentIndex = pageOrder.indexOf(scroller.getActiveLink());
+        const nextIndex =
+          e.deltaY > 0
+            ? currentIndex + 1
+            : currentIndex - 1 >= 0
+            ? currentIndex - 1
+            : 0;
+        const nextElement = pageOrder[nextIndex];
+        scroller.scrollTo(nextElement, defaultScrollProps);
+      },
+      {
+        passive: false,
+      }
+    );
+
+    // cleanup
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
     <main>
       <PageSlider />
 
-      <div className="h-screen w-screen bg-red-500" id="hero">
-        HERO
-      </div>
+      <Element name="hero">
+        <div className="h-screen w-screen bg-red-500">HERO</div>
+      </Element>
 
       <div className="h-screen w-screen bg-green-500" id="about">
         ABOUT
