65fe7cc5cf17958cdae5ff60552f598325b7d717
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-09-29 21:17:17 -0300

    refactor: atomic design structure

---
+++ src/components/organism/PageSlider.tsx
@@ -0,0 +1,44 @@
+import { Link as ScrollLink } from "react-scroll";
+
+const PageSlider = () => {
+  return (
+    <div className="fixed top-[50%] right-0 mr-2 z-50 translate-x-[-50%] translate-y-[-50%]">
+      <div className="d-flex flex-column">
+        <ScrollLink
+          className="h-10 w-2 bg-gray-200/50 rounded-full flex items-center justify-center mb-2 cursor-pointer"
+          activeClass="bg-red-500/100"
+          to="hero"
+          spy={true}
+          smooth={true}
+          duration={1000}
+        />
+        <ScrollLink
+          className="h-10 w-2 bg-gray-200/50 rounded-full flex items-center justify-center mb-2 cursor-pointer"
+          activeClass="bg-red-500/100"
+          to="tech"
+          spy={true}
+          smooth={true}
+          duration={1000}
+        />
+        <ScrollLink
+          className="h-10 w-2 bg-gray-200/50 rounded-full flex items-center justify-center mb-2 cursor-pointer"
+          activeClass="bg-red-500/100"
+          to="exp"
+          spy={true}
+          smooth={true}
+          duration={1000}
+        />
+        <ScrollLink
+          className="h-10 w-2 bg-gray-200/50 rounded-full flex items-center justify-center mb-2 cursor-pointer"
+          activeClass="bg-red-500/100"
+          to="proj"
+          spy={true}
+          smooth={true}
+          duration={1000}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageSlider;
